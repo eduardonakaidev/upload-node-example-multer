c47601821454d24ac4d4677e2db0923186497bed
eduardo i nakai frasson <109884797+eduardonakaidev@users.noreply.github.com>
2023-12-10 17:35:43 -0300

    first commit

---
+++ index.ts
@@ -0,0 +1,15 @@
+import  express  from "express";
+import multer from "multer";
+import {storage} from "./multerConfig";
+
+
+const upload = multer({storage:storage});
+const app = express();
+
+app.use('/files',express.static("uploads"))
+app.post('/uploads',upload.single("file"),(req,res)=>{
+    return res.json(req.file?.filename); 
+})
+
+
+app.listen(3000);
